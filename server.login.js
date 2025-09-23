8e5811806e5f432bf109de277dda0b7a276f9e17
mildlymadscientist <enhackler@wpi.edu>
2025-09-23 13:34:58 -0400

    Server connects to right db but only uses test pw. Does not check authentication, just uses base mongodb guide w/ express.

--- server.login.js
+++ server.login.js
@@ -1,6 +1,22 @@
+require('dotenv').config();
+
+const express = require('express'),
+  app = express(),
+  dreams = []
+
+app.use(express.static('public'))
+app.use(express.static('views'))
+
+app.use(express.json())
+
+app.post('/submit', (req, res) => {
+  dreams.push(req.body.newdream)
+  res.writeHead(200, { 'Content-Type': 'application/json' })
+  res.end(JSON.stringify(dreams))
+})
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://tester:eKrISt8Wohc2sPAS@a3.kngbxj2.mongodb.net/?retryWrites=true&w=majority&appName=A3";
+const uri = `mongodb+srv://${process.env.USERNM}:${process.env.PASS}@${process.env.HOST}/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -11,16 +27,43 @@ const client = new MongoClient(uri, {
   }
 });
 
+let collection = null
+
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    await client.connect(
+      err => {
+        console.log("err :", err);
+        client.close();
+      }
+
+    );
+    collection = client.db("blogDatabase").collection("blogUsers");
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    await client.db("blogDatabase").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
   }
 }
+
+app.use((req, res, next) => {
+  if (collection !== null) {
+    next()
+  } else {
+    res.status(503).send()
+  }
+})
+
+app.get("/docs", async (req, res) => {
+  if (collection !== null) {
+    const docs = await collection.find({}).toArray()
+    res.json(docs)
+  }
+})
+
 run().catch(console.dir);
+
+app.listen(process.env.PORT || 3000)	
